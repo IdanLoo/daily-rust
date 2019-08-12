7437e16605d972bc51d5c7542da04765077c56b6
Idan Loo <im@siwei.lu>
2019-08-12 18:22:38 +0800

    feat(198): init

---
+++ src/house_robber.rs
@@ -0,0 +1,13 @@
+/**
+ * 198. House Robber
+ * URL: https://leetcode-cn.com/problems/house-robber/
+ * Author: Idan Loo <im@siwei.lu>
+ * Date: 2019-8-12
+ */
+pub struct Solution {}
+
+impl Solution {
+  pub fn rob(nums: Vec<i32>) -> i32 {
+    nums[0]
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod add_two_numbers;
 pub mod find_median_sorted_arrays;
+pub mod house_robber;
 pub mod length_of_longest_substring;
 pub mod rotting_oranges;
 
