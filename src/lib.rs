e09592b2844071763e20bd11384de650b265ac94
Idan Loo <im@siwei.lu>
2019-08-15 11:39:29 +0800

    feat(1028): done

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod add_two_numbers;
 pub mod find_median_sorted_arrays;
 pub mod house_robber;
 pub mod length_of_longest_substring;
+pub mod recover_a_tree_from_preorder_traversal;
 pub mod rotting_oranges;
 
 #[cfg(test)]
